5a8569bb99d158e83d6425e4706c846d3d62ce73
subhalagnanahak <nahaksubhalagna@gmail.com>
2026-06-04 20:41:50 +0530

    Added assignment1 files

---
+++ src/assingment1/java/assinment2.java
@@ -0,0 +1,27 @@
+package assingment1.java;
+
+import java.util.Scanner;
+
+public class assinment2 {
+//    Assignment 2: Input from User
+//    Take input:
+//    Name
+//    Age
+//    College
+
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter name of students: ");
+        String name = input.nextLine();
+        System.out.print("Enter age of the students: ");
+        int age = input.nextInt();
+        System.out.print("Enter college of the students: :");
+        String college = input.nextLine();
+        input.nextLine(); // consume the leftover Enter key
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("college: " + college);
+
+    }
+}
---
+++ src/assingment1/java/assinment5.java
@@ -0,0 +1,30 @@
+package assingment1.java;
+
+import java.util.Scanner;
+
+public class assinment5 {
+    static void main(String[] args) {
+//        Assignment 5: Largest Number
+//        Take 3 numbers.
+//                Find largest using if-else.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter number");
+        int num2 = sc.nextInt();
+        System.out.println("Enter number");
+        int num3 = sc.nextInt();
+
+            if (num1 >= num2 && num1 >= num3) {
+                System.out.println("Largest Number = " + num1);
+            }
+            else if (num2 >= num1 && num2 >= num3) {
+                System.out.println("Largest Number = " + num2);
+            }
+            else {
+                System.out.println("Largest Number = " + num3);
+            }
+
+        }
+    }
