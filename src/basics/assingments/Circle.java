608a4cbf7e231c436c7f2b5a064410ebb05b04c3
subhalagnanahak <nahaksubhalagna@gmail.com>
2026-07-08 08:37:31 +0530

    circle area perimeter

---
+++ src/basics/assingments/Circle.java
@@ -0,0 +1,16 @@
+package basics.assingments;
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // circle AREA  formula = pie * (r*r)
+
+        System.out.print("Enter radius: ");
+        double radius = input.nextDouble();
+
+        System.out.print("Area of Circle: " + (Math.PI * Math.pow(radius, 2)));
+    }
+
+}
