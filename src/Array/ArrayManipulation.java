d917636cfbc6582dc14d7b4111852baf769d8d5c
subhalagnanahak <nahaksubhalagna@gmail.com>
2026-09-13 22:57:11 +0530

    Merge remote-tracking branch 'origin/master'

--- src/Array/ArrayManipulation.java
+++ src/Array/ArrayManipulation.java
@@ -1,3 +1,4 @@
+package Array;
 
 public class ArrayManipulation {
     static void ReverseArray(int[] arr) {
@@ -26,10 +27,25 @@ public class ArrayManipulation {
         }
     }
 
-    static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5};
-        ArrayManipulation.ReverseArray(arr);
+    static void rightShift(int[] arr) {
+        int n = arr.length;
+        int temp =  arr[n - 1];
 
+        for (int i = n - 1 ; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = temp;
+    }
+
+
+    static void main( ) {
+        int arr[] = {1, 2, 3, 4, 5};
+       // ArrayManipulation.ReverseArray(arr);
+     rightShift(arr) ;
+         for (int a : arr) {
+             System.out.print(a + " ");
+         }
+        }
 
     }
-}
+
